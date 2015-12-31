c8d35f21c6a758931ef70a03f74eb9a1b4efe97c
afif <komaltechno@gmail.com>
2016-01-01 01:05:59 +0700

    create features

---
+++ spec/Domain/Entity/UserSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Domain\Entity;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class UserSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Domain\Entity\User');
+    }
+}
